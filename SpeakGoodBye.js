411b7a211e556c491da38a70d7df55fc64c9a65a
semabash <82588105+semabash@users.noreply.github.com>
2021-09-14 17:18:28 -0500

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,17 @@
+(function(window){
+		const byeSpeaker = {};
+		const speakWord = "Good Bye";
+
+
+		byeSpeaker.speak = function (name) {
+		  console.log(speakWord + " " + name);
+		}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,20 @@
+
+(function () {
+
+const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (let i = 0; i < names.length; i++) {
+
+
+  const firstLetter = names[i].charAt(0).toLowerCase();
+
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
